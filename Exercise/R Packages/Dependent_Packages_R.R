27863914047ff0e414b7ba2339d8c5c4546033a2
Jacob Cochran <78105426+jcochran-usfws@users.noreply.github.com>
2024-11-29 16:25:36 -0500

    Updated 2024 Quarto Snowy Plover Exercise
    New files associated with the updated exercise for Quarto.

---
+++ Exercise/R Packages/Dependent_Packages_R.R
@@ -0,0 +1,19 @@
+### Packages Needed for R Markdown NCTC Training
+
+# Create vector of packages
+
+requiredPackages <- c("tidyverse", "knitr", "officedown", "officer", "lubridate", "readxl", "janitor", 
+                      "kableExtra", "flextable", "rmarkdown", "mapview", "sf", "gt", "purrr", "english", "maps")
+
+# Function to install and load any packages not installed
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+# Load packages
+
+ipak(requiredPackages)
